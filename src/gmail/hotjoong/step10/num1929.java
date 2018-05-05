9438b7ee94349a3b80e569a0fc677f5a7b4ab5e8
kimhyeonjung <hotjoong@gmail.com>
2018-05-05 12:52:45 +0900

    백준

--- src/gmail/hotjoong/step10/num1929.java
+++ src/gmail/hotjoong/step10/num1929.java
@@ -3,43 +3,35 @@ package gmail.hotjoong.step10;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class num2581 {
+public class num1929 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int M = sc.nextInt();
+		if(M==1) {
+			M=2;
+		}
 		int N = sc.nextInt();
 		ArrayList<Integer> number = new ArrayList();
+		ArrayList<Integer> result = new ArrayList();
 		boolean check=true;
 		int min=N;
-		int sum=0;;
 		for(int i=M; i<=N; i++) {
 			number.add(i);
 		}
-		int c=0;
 		for(int i=0; i<=N-M; i++) {
 			for(int j=2; j<number.get(i); j++) {
 				if(number.get(i)%j==0)
 					check = false; 
 			}
 			if(check) {
-				if(min>number.get(i)&&number.get(i)!=1)
-					min=number.get(i);
-				sum+= number.get(i);
-				c++;
+				result.add(number.get(i));
 			}
-			check = true;
+			check=true;
 		}
-		if(number.contains(1)) {
-			sum--;
-			c--;
-		}
-		if(c==0)
-			System.out.println(-1);
-		else {
-			System.out.println(sum);
-			System.out.println(min);
-		}
+
+		for(int i=0; i<result.size(); i++)
+			System.out.println(result.get(i));
 	}
 
 }
