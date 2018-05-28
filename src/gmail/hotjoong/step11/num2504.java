c2f47b6479507d54ab0c092a68881c1fb7965b0f
kimhyeonjung <hotjoong@gmail.com>
2018-05-28 17:53:28 +0900

    백준

--- src/gmail/hotjoong/step11/num2504.java
+++ src/gmail/hotjoong/step11/num2504.java
@@ -1,10 +1,42 @@
 package gmail.hotjoong.step11;
 
+import java.util.Scanner;
+
 public class num2504 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String bracket = sc.next();
+		int result=-1;
+		
+		
+		System.out.println(bracket_check(bracket));
 	}
+
+	private static int bracket_check(String bracket) {
+		//올바른 괄호인지 검
+		String bracket2 = bracket;
+		while(bracket2.contains("()")||bracket2.contains("[]")) {
+			bracket2 = bracket.replace("()", "");
+			bracket2 = bracket.replace("[]", "");
+		}
+		if(bracket2.length()!=0)
+			return 0; //올바른 괄호가 아니면 0리턴
+		
+		while(bracket.contains("()")||bracket.contains("[]")) {
+			bracket.replace("()", "2");
+			bracket.replace("()", "3");
+		}
+		while(bracket.contains("(")||bracket.contains(")")||bracket.contains("[")||bracket.contains("]")) {
+			
+		}
+		
+		
+		
+		
+		
+		return 0;
+	}
+	
 
 }
